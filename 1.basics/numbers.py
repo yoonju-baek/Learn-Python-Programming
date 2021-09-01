793bf562db079e839a6ff91c4785c9ac85857a84
Yoonju Baek <mbibe91@gmail.com>
2021-09-01 18:29:53 -0500

    operator and str()

---
+++ 1.basics/numbers.py
@@ -0,0 +1,9 @@
+# Operator
+# add(+), subtract(-), multiply(*), divide(/)
+# exponents(**)
+
+# str() function - convert the non-string values into an string
+# if don't use it, Type Errors occurs
+age = 25
+str_message = "Happy " + str(age) + "th Birthday!"
+print(str_message)
