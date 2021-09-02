02d5ead27b3ec1ca682f188ae5a0e77682656484
Yoonju Baek <mbibe91@gmail.com>
2021-09-01 21:29:16 -0500

    lists

---
+++ 1.basics/lists.py
@@ -0,0 +1,15 @@
+cars = ['Audi', 'BMW', 'Cadillac', 'Chevrolet', 'Mazda', 'Tesla']
+
+print(cars)
+
+# Access the individual items in a list
+# first element
+print('first element: ' + cars[0])
+
+# last element
+print(cars[5])
+print('last element: '+ cars[-1])
+
+# access second last element
+print('second last element: '+ cars[-2])
+
