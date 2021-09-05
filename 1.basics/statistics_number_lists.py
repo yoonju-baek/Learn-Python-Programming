71271dbd72fd7df7968257011c55f227c1068b8f
Yoonju Baek <mbibe91@gmail.com>
2021-09-04 20:37:08 -0500

    manipulating lists

---
+++ 1.basics/statistics_number_lists.py
@@ -0,0 +1,9 @@
+# Statistics with a list of numbers
+# min() - retrive minimum number from a list
+# max() - retrive maximum number from a list
+# sum() - sum of a list
+
+numbers = list(range(0,11))
+print('minimum number: ' + str(min(numbers)))
+print('maximum number: ' + str(max(numbers)))
+print('sum : ' + str(sum(numbers)))
