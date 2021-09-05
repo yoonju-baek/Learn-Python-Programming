91e1a11f849c494e7524fa96742c23830e37b10f
Yoonju Baek <mbibe91@gmail.com>
2021-09-04 20:19:22 -0500

    numerical lists

---
+++ 1.basics/numerical_lists.py
@@ -0,0 +1,13 @@
+# Numerical lists
+# list() function - creates a list
+# range() function - generate a series of numbers
+# range(start_number, end_number)
+# range(start_number, end_number, step) 
+numbers = list(range(1,5))
+print(numbers)
+
+for value in range(1,5):
+    print(value)
+
+any_numbers = list(range(1,11,3))
+print(any_numbers) 
