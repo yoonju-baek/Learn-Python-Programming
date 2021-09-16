de7c5663a6ec85e98a689be90a8c7971db2e1af5
Yoonju Baek <mbibe91@gmail.com>
2021-09-16 17:52:10 -0500

    manipulate an arbitrary number in function

---
+++ functions_arbitrary_number.py
@@ -0,0 +1,32 @@
+# Manupulate an arbitrary number of arguments
+def make_bubble_tea(tea, *requests):
+    """
+    Print the list of toppings that have been requested.
+    """
+    print("\nYour own bubble tea: " + tea.title())
+    for request in requests:
+        print(request)
+
+make_bubble_tea('milk tea', 'less sugar', 'normal ice', 'kiwi', 'tapioca')
+
+# Arbitary keyword arguments
+def user_profile(first, last, **add_info):
+    """
+    Build a dictionary containing user information
+    """
+    profile = {}
+    profile['firstname'] = first
+    profile['lastname'] = last
+
+    for key, value in add_info.items():
+        profile[key] = value
+    
+    return profile
+
+user = user_profile('john', 'smith',
+                    city='calgary',
+                    phone='6548397263',
+                    email='john.s@gmail.com')
+
+print(user)
+
