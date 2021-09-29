c92aa01019504819c2a26b033121c65cd77eda1b
Yoonju Baek <mbibe91@gmail.com>
2021-09-28 20:04:00 -0500

    creating a window

---
+++ 2. projects/Alien Invasion/alien_invasion.py
@@ -0,0 +1,26 @@
+import sys
+import pygame
+
+def run_game():
+    # Initialize game and create a screen object
+    pygame.init() # Initialize background settings that Pygame needs to work properly.
+    screen = pygame.display.set_mode((1200, 800)) # set create a display window(screen or surface) (1200(w) x 800(h) pixels)
+    pygame.display.set_caption("Alien Invasion")
+
+    # Set the background color.
+    bg_color = (230, 230, 230)
+
+    # Start the main loop for the game
+    while True:
+        # Watch for keyboard and mouse events.
+        for event in pygame.event.get(): # detect and access te events
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+        # Redraw the screen during each pass through the loop.
+        screen.fill(bg_color)
+        
+        # Make the most recently drawn screen visible.
+        pygame.display.flip()
+
+run_game()
