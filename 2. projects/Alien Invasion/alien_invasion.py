318fdca837fed1ca2624366acd198e5eaf30231a
Yoonju Baek <mbibe91@gmail.com>
2021-09-28 20:34:59 -0500

    create settings

--- 2. projects/Alien Invasion/alien_invasion.py
+++ 2. projects/Alien Invasion/alien_invasion.py
@@ -1,14 +1,18 @@
 import sys
 import pygame
 
+from settings import Settings
+
 def run_game():
-    # Initialize game and create a screen object
+    # Initialize pygame, settings, and create a screen object
     pygame.init() # Initialize background settings that Pygame needs to work properly.
-    screen = pygame.display.set_mode((1200, 800)) # set create a display window(screen or surface) (1200(w) x 800(h) pixels)
+    ai_settings = Settings()
+
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height)) # set create a display window(screen or surface) (1200(w) x 800(h) pixels)
     pygame.display.set_caption("Alien Invasion")
 
     # Set the background color.
-    bg_color = (230, 230, 230)
+    #bg_color = (230, 230, 230)
 
     # Start the main loop for the game
     while True:
@@ -18,8 +22,9 @@ def run_game():
                 sys.exit()
 
         # Redraw the screen during each pass through the loop.
-        screen.fill(bg_color)
-        
+        #screen.fill(bg_color)
+        screen.fill(ai_settings.bg_color)
+
         # Make the most recently drawn screen visible.
         pygame.display.flip()
 
