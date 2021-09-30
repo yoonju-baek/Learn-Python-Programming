9df6f133baa89ed31d492aae54502a2969c7a9fb
Yoonju Baek <mbibe91@gmail.com>
2021-09-30 18:14:48 -0500

    set up ship movement

--- 2. projects/Alien Invasion/alien_invasion.py
+++ 2. projects/Alien Invasion/alien_invasion.py
@@ -2,6 +2,8 @@ import sys
 import pygame
 
 from settings import Settings
+from ship import Ship
+import game_functions as gf
 
 def run_game():
     # Initialize pygame, settings, and create a screen object
@@ -11,21 +13,17 @@ def run_game():
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height)) # set create a display window(screen or surface) (1200(w) x 800(h) pixels)
     pygame.display.set_caption("Alien Invasion")
 
+    # Make a ship.
+    ship = Ship(ai_settings, screen)
+
     # Set the background color.
     #bg_color = (230, 230, 230)
 
     # Start the main loop for the game
     while True:
-        # Watch for keyboard and mouse events.
-        for event in pygame.event.get(): # detect and access te events
-            if event.type == pygame.QUIT:
-                sys.exit()
-
-        # Redraw the screen during each pass through the loop.
-        #screen.fill(bg_color)
-        screen.fill(ai_settings.bg_color)
-
-        # Make the most recently drawn screen visible.
-        pygame.display.flip()
+        gf.check_events(ship)
+        ship.update()
+        gf.update_screen(ai_settings, screen, ship)
+        
 
 run_game()
