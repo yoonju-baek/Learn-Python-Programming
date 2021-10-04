72f4818938145d687195ca7ac6cbe9f3976558de
Yoonju Baek <mbibe91@gmail.com>
2021-10-03 21:14:16 -0500

    creating the fleet of aliens

--- 2. projects/Alien Invasion/alien_invasion.py
+++ 2. projects/Alien Invasion/alien_invasion.py
@@ -18,13 +18,20 @@ def run_game():
 
     # Make a group to store bullets in.
     bullets = Group()
+    aliens = Group()
+
+    # Make an alien.
+    #alien = Alien(ai_settings, screen)
+
+    # Create the fleet of aliens
+    gf.create_fleet(ai_settings, screen, aliens)
 
     # Start the main loop for the game
     while True:
         gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
         gf.update_bullets(bullets)  
-        gf.update_screen(ai_settings, screen, ship, bullets)
+        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
         
 
 run_game()
--- 2. projects/Alien Invasion/game_functions.py
+++ 2. projects/Alien Invasion/game_functions.py
@@ -1,6 +1,7 @@
 import sys
 import pygame
 from bullet import Bullet
+from alien import Alien
 
 def check_keydown_events(event, ai_settings, screen, ship, bullets):
     """Respond to keypresses."""
@@ -42,7 +43,7 @@ def check_events(ai_settings, screen, ship, bullets):
             check_keyup_events(event, ship)
 
 
-def update_screen(ai_settings, screen, ship, bullets):
+def update_screen(ai_settings, screen, ship, aliens, bullets):
     """Update images on the screen and flip to the new screen"""
     # Redraw the screen during each pass through the loop.
     screen.fill(ai_settings.bg_color)
@@ -52,6 +53,7 @@ def update_screen(ai_settings, screen, ship, bullets):
         bullet.draw_bullet()
 
     ship.blitme()
+    aliens.draw(screen) # Group.draw() -Pygame automatically draws each element in the group at the position defined by its rect attribute.
 
     # Make the most recently drawn screen visible.
     pygame.display.flip()
@@ -65,3 +67,21 @@ def update_bullets(bullets):
     for bullet in bullets.copy():
         if bullet.rect.bottom <= 0:
             bullets.remove(bullet) # remove() method to remove the sprite from groups
+
+def create_fleet(ai_settings, screen, aliens):
+    """Create a full fleet of aliens."""
+    # Create an alien and find the number of aliens in a row.
+    # Spacing between each alien is equal to one alien width.
+    alien = Alien(ai_settings, screen)
+    alien_width = alien.rect.width
+    available_space_x = ai_settings.screen_width - 2 * alien_width
+    number_aliens_x = int(available_space_x / (2 * alien_width))
+
+    # Create the first row of aliens.
+    for alien_number in range(number_aliens_x):
+        # Create an alien and place it in a row.
+        alien = Alien(ai_settings, screen)
+        alien.x = alien_width + 2 * alien_width * alien_number
+        alien.rect.x = alien.x
+        aliens.add(alien)
+    
