81e882711ce12d0660fa02fe8aac3d64ebace836
Yoonju Baek <mbibe91@gmail.com>
2021-10-06 16:48:43 -0500

    add the play button

--- 2. projects/Alien Invasion/alien_invasion.py
+++ 2. projects/Alien Invasion/alien_invasion.py
@@ -3,6 +3,7 @@ from pygame.sprite import Group
 
 from settings import Settings
 from game_stats import GameStats
+from button import Button
 from ship import Ship
 import game_functions as gf
 
@@ -13,6 +14,9 @@ def run_game():
 
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height)) # set create a display window(screen or surface) (1200(w) x 800(h) pixels)
     pygame.display.set_caption("Alien Invasion")
+    
+    # Make the Play button
+    play_button = Button(ai_settings, screen, "Play")
 
     # Create an instance to store game statistics.
     stats = GameStats(ai_settings)
@@ -37,8 +41,8 @@ def run_game():
             ship.update()
             gf.update_bullets(ai_settings, screen, ship, aliens, bullets)  
             gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
-            
-        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
+
+        gf.update_screen(ai_settings, screen, stats, ship, aliens, bullets, play_button)
         
 
 run_game()
--- 2. projects/Alien Invasion/game_functions.py
+++ 2. projects/Alien Invasion/game_functions.py
@@ -45,7 +45,7 @@ def check_events(ai_settings, screen, ship, bullets):
             check_keyup_events(event, ship)
 
 
-def update_screen(ai_settings, screen, ship, aliens, bullets):
+def update_screen(ai_settings, screen, stats, ship, aliens, bullets, play_button):
     """Update images on the screen and flip to the new screen"""
     # Redraw the screen during each pass through the loop.
     screen.fill(ai_settings.bg_color)
@@ -57,6 +57,10 @@ def update_screen(ai_settings, screen, ship, aliens, bullets):
     ship.blitme()
     aliens.draw(screen) # Group.draw() -Pygame automatically draws each element in the group at the position defined by its rect attribute.
 
+    # Draw the play button if the game is inactive
+    if not stats.game_active:
+        play_button.draw_button()
+        
     # Make the most recently drawn screen visible.
     pygame.display.flip()
 
@@ -100,7 +104,7 @@ def change_fleet_direction(ai_settings, aliens):
 
 def ship_hit(ai_settings, stats, screen, ship, aliens, bullets):
     """Respond to ship being hit by alien."""
-    if stats.ships_left > 0:
+    if stats.ship_left > 0:
         # Decrement ships left.
         stats.ship_left -= 1
 
