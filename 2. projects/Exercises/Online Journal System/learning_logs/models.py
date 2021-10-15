11db07eb5e135db10f039e53a7ba6ed3008b6ed4
Yoonju Baek <mbibe91@gmail.com>
2021-10-14 20:51:47 -0500

    register and define the entry model

--- 2. projects/Exercises/Online Journal System/learning_logs/models.py
+++ 2. projects/Exercises/Online Journal System/learning_logs/models.py
@@ -3,10 +3,29 @@ from django.db import models
 # Create your models here.
 class Topic(models.Model):
     """A topic the user is learning about"""
-    text = models.CharField(max_length=200) # CharField - small amount of data that's made up of characters, or text.
-    date_added = models.DateTimeField(auto_now_add=True) # DateTimeField - a piece of data will record a date and time.
+    text = models.CharField(max_length=200) 
+    # CharField - small amount of data that's made up of characters, or text.
+
+    date_added = models.DateTimeField(auto_now_add=True)
+    # DateTimeField - a piece of data will record a date and time.
     # auto_now_add=True - to automatically set this attribute to the current date and time.
 
     def __str__(self):
         """Return a string representation of the model."""
         return self.text
+
+class Entry(models.Model):
+    """Something specific learned about a topic"""
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    # TextField - no size limit
+
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    # Meta holds extra information for managing a model
+    class Meta:
+        verbose_name_plural = 'entries'
+
+    def __str__(self):
+        """Return a string representation of the model."""
+        return self.text[:50] + "..."
