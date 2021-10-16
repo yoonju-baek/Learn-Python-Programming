87d7b51f4a657b94c8559180c438e4b8146d35c0
Yoonju Baek <mbibe91@gmail.com>
2021-10-15 20:11:11 -0500

    add the topics page and individual topic pages

--- 2. projects/Exercises/Online Journal System/learning_logs/urls.py
+++ 2. projects/Exercises/Online Journal System/learning_logs/urls.py
@@ -7,4 +7,10 @@ from . import views
 urlpatterns = [
     # Home page
     url(r'^$', views.index, name='index'),
+
+    # Show all topics.
+    url(r'^topic/$', views.topics, name='topics'),
+
+    # Detail page for a sngle topic
+    url(r'^topics/(?P<topic_id>\d+)/$', views.topic, name='topic')
 ]
